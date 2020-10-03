f75595b6af184908bba8ec5cfe1ba077985c6dad
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-03 12:57:18 +0530

    UC4 multiple parameter case implemented

--- src/main/java/MaxFinder.java
+++ src/main/java/MaxFinder.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MaxFinder<T extends Comparable<T>> {
 
-    public T findMax(T a,T b,T c){
-        if(a.compareTo(b)>0&&a.compareTo(c)>0 )
-            return a;
-        if(b.compareTo(a)>0&&b.compareTo(c)>0)
-            return b;
-        return c;
+    @SafeVarargs
+    public final T findMax(T... params){
+        Arrays.sort(params, Comparator.reverseOrder());
+        return params[0];
     }
 }
